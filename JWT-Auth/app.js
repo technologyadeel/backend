103e65e48dbc6e52478b47fbdb2fd9a3e97689bd
technologyadeel <crewraj11@gmail.com>
2023-11-18 22:11:16 +0530

    finish

--- JWT-Auth/app.js
+++ JWT-Auth/app.js
@@ -1,12 +1,17 @@
 require('dotenv').config()
 const express = require('express')
 const cookieParser = require('cookie-parser')
+const cors = require('cors')
 
 const app = express()
 const connectToDb = require('./config/db')
 connectToDb()
 app.use(express.json())
 app.use(cookieParser())
+app.use(cors({
+    origin: [process.env.CLIENT_URL],
+    credentials: true
+}))
 const authRouter = require('./router/authRoute')
 
 app.use('/api/auth/', authRouter)
