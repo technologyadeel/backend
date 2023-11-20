9a391752760f98b1c854dd24b657edb449e21ab7
technologyadeel <crewraj11@gmail.com>
2023-11-20 22:01:04 +0530

    ecommerse-modelling

---
+++ dataModling/models/ecommerce/order.model.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose')
+
+const orderItemSchema = new mongoose.Schema({
+    productId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Product'
+    },
+    quantity: {
+        type: Number,
+        required: true
+    }
+})
+
+const orderSchema = new mongoose.Schema({
+    orderPrice: {
+        type: Number,
+        required: true
+    },
+    customer: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    orderItem: {
+        type: [orderItemSchema]
+    },
+    address: {
+        type: String,
+        required: true
+    },
+    status: {
+        type: String,
+        enum: ['PENDING', 'CANCELLED', 'DELIVERED'], // choices
+        default: 'PENDING'
+    }
+})
+
+module.exports = mongoose.model('Order', orderSchema)
---
+++ dataModling/models/ecommerce/product.model.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose')
+
+const productSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    productImage: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        default: 0
+    },
+    stock: {
+        type: String,
+        default: 0
+    },
+    category: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Category',
+        required: true
+    },
+    owner: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    }
+})
+
+module.exports = mongoose.model('Product', productSchema)
