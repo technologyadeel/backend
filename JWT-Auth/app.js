91ea819f47a398a10ee69c56feacd214d0048148
Adeel Ahmad <129967189+AdeelAhmad45@users.noreply.github.com>
2023-12-13 21:42:12 +0530

    Update app.js

--- JWT-Auth/app.js
+++ JWT-Auth/app.js
@@ -6,12 +6,14 @@ const cors = require('cors')
 const app = express()
 const connectToDb = require('./config/db')
 connectToDb()
+
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
     origin: [process.env.CLIENT_URL],
     credentials: true
 }))
+
 const authRouter = require('./router/authRoute')
 
 app.use('/api/auth/', authRouter)
